190a63fafde5c639f0e05f777c15c7006b73609e
John F. Mercer <john.f.mercer@gmail.com>
2015-05-24 19:48:58 -0400

    moves old files out of the way

--- old-specs/LionSpec.php
+++ old-specs/LionSpec.php
@@ -22,18 +22,28 @@ class LionSpec extends ObjectBehavior
 
     // I couldn't find a way to test the parameter type
     // of eat(Food $food)
-    function it_eats_food(){
+    function it_eats_food(\Food $food)
+    {
+//        $this->eat($food)->getEnergy()->shouldBeEqualTo(3);
         return true;
     }
 
     // Again, I don't know how to test parameter types
     // for Steak, Fish, and Grain
-    function it_eats_all_three_kinds_of_food()
-    {
-        return true;
-    }
+//    function it_eats_all_three_kinds_of_food(\Steak $steak)
+//    {
+//        $this->eat($steak)->getEnergy()->shouldBeEqualTo(3);
+//        return true;
+//    }
+    // eat() doesn't throw an exception when empty. WTF?
+    // Is the type-hinting not working?
+//    function it_should_only_eat_Food()
+//    {
+//        $this->shouldThrow('\ErrorException')->duringEat(null);
+//    }
 
-    function eating_should_add_3_energy(){
+    function eating_should_add_3_energy()
+    {
         $this->eat()->getEnergy()->shouldBeEqualTo(3);
     }
 
