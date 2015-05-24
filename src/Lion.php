5465ded3618ff423e50547718d77655f8f6b5916
John F. Mercer <john.f.mercer@gmail.com>
2015-05-24 15:41:23 -0400

    Lion is growing

--- src/Lion.php
+++ src/Lion.php
@@ -2,7 +2,26 @@
 
 class Lion
 {
+    protected $energy;
+
+    public function __construct()
+    {
+        $this->energy = 0;
+    }
+
     public function speak(){
         return "Grrr";
+    }
+
+    public function play(){
+        return "YE-AH!";
+    }
+
+    public function eat(){
+        $this->energy += 3;
+    }
+
+    public function getEnergy(){
+        return $this->energy;
     }
 }
