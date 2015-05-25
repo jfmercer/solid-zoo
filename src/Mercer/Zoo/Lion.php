d170e5f9bd51fa0d89caad6503afa3421c67b232
John F. Mercer <john.f.mercer@gmail.com>
2015-05-24 20:50:40 -0400

    I have methods I can't test. Frustrating.

--- src/Mercer/Zoo/Lion.php
+++ src/Mercer/Zoo/Lion.php
@@ -6,11 +6,15 @@ class Lion
 {
     protected $energy;
 
+    protected static $number_of_lions = 0;
+
     public function __construct()
     {
         $this->energy = 0;
+        $this::$number_of_lions += 1;
     }
 
+    // Can't test parameter types
     public function eat(Food $food)
     {
         $this->energy += 3;
@@ -38,5 +42,14 @@ class Lion
 
     public function getEnergy(){
         return $this->energy;
+    }
+
+    public function getNumberOfLions(){
+        return $this::$number_of_lions;
+    }
+
+    // Can't test
+    public function __destroy(){
+        $this::$number_of_lions -= 1;
     }
 }
