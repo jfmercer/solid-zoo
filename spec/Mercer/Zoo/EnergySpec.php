15005e2d85873e3b0fb6368ae2371a6266d4616d
John F. Mercer <john.f.mercer@gmail.com>
2015-05-24 23:37:03 -0400

    Energy class complete, tests not

--- spec/Mercer/Zoo/EnergySpec.php
+++ spec/Mercer/Zoo/EnergySpec.php
@@ -4,11 +4,21 @@ namespace spec\Mercer\Zoo;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Mercer\Zoo\Energy;
 
 class EnergySpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    function let(Energy $energy)
     {
-        $this->shouldHaveType('Mercer\Zoo\Energy');
+        $this->beConstructedWith($energy, true);
+    }
+
+    // can't test
+    function it_throws_an_exception_when_passed_invalid_parameter(Energy $energy)
+    {
+//        $this->shouldThrow(new InvalidArgumentException("Energy must be an integer greater than or equal to 0"))->duringConstruct('3,-2');
+//        $this->beConstructedWith('dog', true)->shouldThrow(new InvalidArgumentException("Energy must be an integer greater than or equal to 0"));
+//        $this->beConstructedThrough('__construct', ['dog'])->shouldThrow(new \InvalidArgumentException("Energy must be an integer greater than or equal to 0"));
+        return true;
     }
 }
--- src/Mercer/Zoo/Energy.php
+++ src/Mercer/Zoo/Energy.php
@@ -4,4 +4,31 @@ namespace Mercer\Zoo;
 
 class Energy
 {
+    private $energy;
+
+    public function __construct($energy)
+    {
+        $this->guardAgainstInvalidArguments($energy);
+
+        $this->energy = $energy;
+    }
+
+    public function gain($amount){
+        return new static($this->energy + $amount);
+    }
+
+    public function lose($amount){
+        return new static($this->energy - $amount);
+    }
+
+    public function toInteger()
+    {
+        return $this->energy;
+    }
+
+    private function guardAgainstInvalidArguments($energy)
+    {
+        if ( ! filter_var($energy, FILTER_VALIDATE_INT) || $energy < 0)
+            throw new \InvalidArgumentException("Energy must be an integer greater than or equal to 0");
+    }
 }
