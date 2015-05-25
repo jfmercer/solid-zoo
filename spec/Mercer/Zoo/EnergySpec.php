94a105cd7714ff8a2d32ca7248ab1757e81ba691
John F. Mercer <john.f.mercer@gmail.com>
2015-05-24 22:06:03 -0400

    adds Energy class

---
+++ spec/Mercer/Zoo/EnergySpec.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace spec\Mercer\Zoo;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class EnergySpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Mercer\Zoo\Energy');
+    }
+}
