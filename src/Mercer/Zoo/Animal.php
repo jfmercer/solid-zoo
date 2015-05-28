426fd3b5f77d12838ddb3f70e5fb9a316b9a2079
John F. Mercer <john.f.mercer@gmail.com>
2015-05-27 20:29:39 -0400

    added constants to Animal interface

--- src/Mercer/Zoo/Animal.php
+++ src/Mercer/Zoo/Animal.php
@@ -7,6 +7,9 @@ interface Animal {
     const EAT_ENERGY_DEFAULT = 3;
     const REST_ENERGY_DEFAULT = 8;
     const PLAY_ENERGY_DEFAULT = 5;
+    const SPEAK_WORD_DEFAULT = "Grrr";
+    const TIRED_WORD_DEFAULT = "I'm tired";
+    const PLAY_WORD_DEFAULT = "YE-AH!";
 
     public function eat(Food $food);
     public function rest();
